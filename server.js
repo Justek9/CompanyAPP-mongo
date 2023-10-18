3623ed277e01725e8c28454d40d3f7a072b76aca
Justyna Konikiewicz <zagorskaj@wp.pl>
2023-10-18 20:47:32 +0200

    rewrite products routes to use mongoose

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 
 const employeesRoutes = require('./routes/employees.routes')
 const departmentsRoutes = require('./routes/departments.routes')
-// const productsRoutes = require('./routes/products.routes')
+const productsRoutes = require('./routes/products.routes')
 
 const app = express()
 
