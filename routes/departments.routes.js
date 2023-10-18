bfc5962b77e214e4a27227ed6dd71f5d8218910c
Justyna Konikiewicz <zagorskaj@wp.pl>
2023-10-18 21:10:22 +0200

    modify endpoints to send back modified or deleted doc in departmnts routes

--- routes/departments.routes.js
+++ routes/departments.routes.js
@@ -50,7 +50,8 @@ router.put('/departments/:id', async (req, res) => {
 		const dep = await Department.findById(req.params.id)
 		if (dep) {
 			await Department.updateOne({ _id: req.params.id }, { $set: { name: name } })
-			res.json({ message: 'OK' })
+			const updated = await Department.findById(req.params.id)
+			res.json(updated)
 		} else res.status(404).json({ message: 'Not found...' })
 	} catch (err) {
 		res.status(500).json({ message: err })
@@ -62,7 +63,7 @@ router.delete('/departments/:id', async (req, res) => {
 		const dep = await Department.findById(req.params.id)
 		if (dep) {
 			await Department.deleteOne({ _id: req.params.id })
-			res.json({ message: 'OK' })
+			res.send(dep)
 		} else res.status(404).json({ message: 'Not found...' })
 	} catch (err) {
 		res.status(500).json({ message: 'OK' })
--- routes/employees.routes.js
+++ routes/employees.routes.js
@@ -62,7 +62,7 @@ router.delete('/employees/:id', async (req, res) => {
 		if (emp) {
 			await Employee.deleteOne({ _id: req.params.id })
 			res.json({ message: 'OK' })
-		} else res.status(404).json({ message: 'Not found...' })
+			} else res.status(404).json({ message: 'Not found...' })
 	} catch (err) {
 		res.status(500).json({ message: 'OK' })
 	}
